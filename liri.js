ffdb361cb394527c147ab3cb1f92b61ca1c5e1d8
dogray7722 <david.gray61@du.edu>
2017-07-18 22:57:04 -0600

    Got the twitter stuff working.

--- liri.js
+++ liri.js
@@ -1,23 +1,55 @@
 
-var twitter = require("twitter");
-var spotify = require("spotify-node-api");
+//Define the modules
+
 var keys = require('./keys');
+var Twitter = require("twitter");
+// var spotify = require("spotify-node-api");
+var request = require('request');
 
-//Below is the pattern to make API calls:
-// twitterKeys.get(path, params, callback);
+//Provide authentication credentials
 
-
-//What are the various parameters I can choose from-- Check the twitter API Documentation
-//What arguments does the call back function take?
-//What is my screen name?
-//How do I pass a parameter count of 20 tweets?
-//With node, all the functions should be declared as variables
-//Make use of the return statement
-
-
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) {
-        console.log(tweets);
-    }
+var client = new Twitter({
+    consumer_key: keys.twitterKeys.consumer_key,
+    consumer_secret: keys.twitterKeys.consumer_secret,
+    access_token_key: keys.twitterKeys.access_token_key,
+    access_token_secret: keys.twitterKeys.access_token_secret
 });
+
+//Create the function to get the tweets
+
+var getTweets = function() {
+
+    var params = {screen_name: "DavidGray61", count: 20};
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < params.count; i++ ) {
+                console.log(tweets[i].text, tweets[i].created_at);
+            }
+        } else {
+            console.log(error);
+        }
+    });
+}
+
+var input = process.argv;
+var command = input[2];
+
+if (command == "my-tweets") {
+    getTweets();
+}
+
+else if (command == "") {
+
+}
+
+else if (command == "") {
+
+}
+
+else if (command == "") {
+
+}
+
+else {
+    console.log("Invalid command.")
+}
