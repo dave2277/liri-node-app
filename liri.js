3c091d442f08bd21dc291c30b046d77114a1d3ce
dogray7722 <david.gray61@du.edu>
2017-07-21 10:28:29 -0600

    Spotify API working, but still need to do the Ace of Base default song.

--- liri.js
+++ liri.js
@@ -3,11 +3,10 @@
 
 var keys = require('./keys');
 var Twitter = require("twitter");
-// var spotify = require("spotify-node-api");
+var Spotify = require("node-spotify-api");
 var request = require('request');
 
-//Provide authentication credentials
-
+//Provide authentication credentials for twitter
 var client = new Twitter({
     consumer_key: keys.twitterKeys.consumer_key,
     consumer_secret: keys.twitterKeys.consumer_secret,
@@ -15,8 +14,17 @@ var client = new Twitter({
     access_token_secret: keys.twitterKeys.access_token_secret
 });
 
-//Create the function to get the tweets
+//Provide credentials for spotify app
+var spotify = new Spotify({
+    id: 'c2a92198b88d478d895976de3e190c92',
+    secret: '5f64767ea3e24c71907b0539ec5440a3'
+});
 
+//Set the initial input property as a variable for the command
+var input = process.argv;
+var command = input[2];
+
+//Create the function to get the tweets
 var getTweets = function() {
 
     var params = {screen_name: "DavidGray61", count: 20};
@@ -29,24 +37,42 @@ var getTweets = function() {
             console.log(error);
         }
     });
-}
+};
 
-var input = process.argv;
-var command = input[2];
+//Create the function to get the songs
+var getSong = function () {
+    var song = input[3];
+
+    spotify.search({type: 'track', query: song, limit: 1}, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        // console.log(data);
+        console.log("Artist(s) Name: " + data.tracks.items[0].album.artists[0].name);
+        console.log("Song Title: " + data.tracks.items[0].name);
+        console.log("Preview URL: " + data.tracks.items[0].preview_url);
+        console.log("Album: " + data.tracks.items[0].album.name);
+    });
+
+};
+
+
 
 if (command == "my-tweets") {
     getTweets();
 }
 
-else if (command == "") {
+else if (command == "spotify-this-song") {
+    getSong();
 
 }
 
-else if (command == "") {
+else if (command == "movie-this") {
 
 }
 
-else if (command == "") {
+else if (command == "do-what-it-says") {
 
 }
 
